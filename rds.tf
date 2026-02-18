708bdcd032c1eddac5ccb594cbe8a7e04757beac
Reasy94 <desalvialuca@yahoo.it>
2026-02-18 13:50:45 +0100

    rds.tf added

---
+++ rds.tf
@@ -0,0 +1,45 @@
+resource "random_password" "db_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name        = "rag/rds/credentials-v1"
+  description = "RAG DB Vector Credentials"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials_val" {
+  secret_id     = aws_secretsmanager_secret.db_credentials.id
+  secret_string = jsonencode({
+    username = "ragadmin"
+    password = random_password.db_password.result
+    engine   = "postgres"
+    host     = aws_db_instance.rag_db.address
+    port     = 5432
+    db_name  = "ragdb"
+  })
+}
+
+resource "aws_db_instance" "rag_db" {
+  allocated_storage      = 20
+  db_name                = "ragdb"
+  engine                 = "postgres"
+  engine_version         = "16.1"
+  instance_class         = "db.t3.micro"
+  
+  username               = "ragadmin"
+  password               = random_password.db_password.result
+
+  parameter_group_name   = "default.postgres16"
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name  
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  tags = {
+    Environment = "Dev"
+    Project     = "RAG-AWS"
+  }
+}
