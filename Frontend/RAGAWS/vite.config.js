3820fefedbd69e26fba455b9f1e1b1922eaafe97
Reasy94 <desalvialuca@yahoo.it>
2026-03-27 13:40:47 +0100

    other fix

--- Frontend/RAGAWS/vite.config.js
+++ Frontend/RAGAWS/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- Aggiungi questa riga
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <-- E aggiungi questo ai plugin
+    tailwindcss(),
   ],
 })
